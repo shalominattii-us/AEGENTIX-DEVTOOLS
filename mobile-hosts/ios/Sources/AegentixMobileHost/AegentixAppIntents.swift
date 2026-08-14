d62d4cb068721228a7c9fcd50195e83dc6a1e4ff
shalominattii-us <shalominattii.us@gmail.com>
2026-08-14 17:23:56 -0600

    fix(ios): remove iOS 26-only IntentModes from iOS 17 host target

--- mobile-hosts/ios/Sources/AegentixMobileHost/AegentixAppIntents.swift
+++ mobile-hosts/ios/Sources/AegentixMobileHost/AegentixAppIntents.swift
@@ -3,12 +3,11 @@ import AppIntents
 @available(iOS 17.0, *)
 public struct AegentixHeartbeatIntent: AppIntent {
     public static let title: LocalizedStringResource = "AEGENTIX Heartbeat"
-    public static let supportedModes: IntentModes = [.background, .foreground(.dynamic)]
 
     public init() {}
 
     public func perform() async throws -> some IntentResult {
-        // Wire the configured host instance here in the containing iOS app.
+        // The containing iOS app wires the configured host instance.
         return .result()
     }
 }
